4c01bb5b3b6663ee13eee92f0c4941cc10d40e3f
msaeiid <mskarbaschian@gmail.com>
2024-02-27 22:43:05 +0330

    1:28:00

--- Posts/urls.py
+++ Posts/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('homepage/', views.homepage, name='homepage'),
     path('list_posts/', views.list_posts, name='list_posts'),
     path('post_detail/<int:pk>', views.post_detail, name='post_detail'),
+    path('post_update/<int:pk>', views.post_update, name='post_update'),
+    path('post_delete/<int:pk>', views.post_delete, name='post_delete'),
 ]
--- Posts/views.py
+++ Posts/views.py
@@ -1,16 +1,11 @@
-from django.http import HttpRequest, JsonResponse, HttpResponse
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from Posts.models import Post
+from Posts.serializers import PostSerializer
 
-
-## First
-# def homepage(request: HttpRequest):
-#     response = {
-#         'message': 'Hello World!'
-#     }
-#     return JsonResponse(response)
 
 @api_view(["GET", "POST"])
 def homepage(request: Request):
@@ -23,28 +18,42 @@ def homepage(request: Request):
         return Response(data=response, status=status.HTTP_201_CREATED)
 
 
-posts_list_json = [
-    {"id": 1, "title": "My first post", "content": "This is my first"},
-    {"id": 2, "title": "My second post", "content": "This is my second"},
-    {"id": 3, "title": "My third post", "content": "This is my third"},
-]
-
-
-@api_view(["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def list_posts(request: Request):
     if request.method == "GET":
-        response = {'posts': posts_list_json}
+        posts = Post.objects.all()
+        serializers = PostSerializer(instance=posts, many=True)
+        response = {'message': 'posts...', 'data': serializers.data}
         return Response(data=response, status=status.HTTP_200_OK)
+    if request.method == "POST":
+        data = request.data
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+        response = {'message': 'post created', 'data': serializer.data}
+        return Response(data=response, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET"])
 def post_detail(request: Request, pk: int):
-    # pk = int(request.query_params['pk'])
-    response = None
-    for post in posts_list_json:
-        if post['id'] == pk:
-            response = {'response': post}
-    if response is None:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    else:
-        return Response(data=response, status=status.HTTP_200_OK)
+    post = get_object_or_404(Post, id=pk)
+    serializer = PostSerializer(instance=post)
+    response = {'message': 'requested post is :', 'data': serializer.data}
+    return Response(data=response, status=status.HTTP_200_OK)
+
+
+@api_view(http_method_names=['PUT'])
+def post_update(request: Request, pk: int):
+    post = get_object_or_404(Post, id=pk)
+    serializer = PostSerializer(data=request.data, instance=post)
+    if serializer.is_valid():
+        serializer.save()
+        response = {'message': 'Post updated successfully', 'data': serializer.data}
+    return Response(data=response, status=status.HTTP_200_OK)
+
+
+@api_view(http_method_names=['DELETE'])
+def post_delete(request: Request, pk: int):
+    post = get_object_or_404(Post, id=pk)
+    post.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
