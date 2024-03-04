99f705c4367e3d15e3edc7ab6d415ed70972374f
msaeiid <mskarbaschian@gmail.com>
2024-03-05 02:43:24 +0330

    5:05:35

--- Posts/tests.py
+++ Posts/tests.py
@@ -1,8 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIRequestFactory
-from .views import PostListCreateView
+from rest_framework.test import APITestCase, APIClient
 
 User = get_user_model()
 
@@ -15,28 +14,38 @@ class HelloWorldTestCase(APITestCase):
         self.assertEqual(response.data['message'], "Hello World!")
 
 
+USER_CREDENTIAL = {
+    "password": "77002783",
+    "email": "admin@gmail.com",
+    "username": "admin"
+}
+
+
 class PostListCreateTestCase(APITestCase):
 
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.view = PostListCreateView.as_view()
-        self.url = reverse('posts')
-        self.user = User.objects.create(username='test',
-                                        email='test@gmail.com',
-                                        password='77002783Sas@#')
+        self.client = APIClient()
+
+    def authenticate(self):
+        self.client.post(reverse('signup'), USER_CREDENTIAL)
+        USER_CREDENTIAL.pop('username')
+        response = self.client.post(reverse('token_obtain_pair'), USER_CREDENTIAL)
+
+        token = response.data['access']
+        self.client.force_authenticate(user=User.objects.get(USER_CREDENTIAL['email']), token=token)
+        # self.client.credentials(HTTP_AUTHORIZATION=f'bearer {token}')
 
     def test_list_posts(self):
-        request = self.factory.get(self.url)
-        response = self.view(request)
+        response = self.client.get(reverse('posts'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_creation(self):
-        test_post_data = {
+        self.authenticate()
+
+        sample_data = {
             "title": "sample title",
             "content": "sample content",
-            #"author": self.user.id
+            "author": User.objects.get(email=USER_CREDENTIAL['email']).id
         }
-        request = self.factory.post(self.url, test_post_data)
-        request.user = self.user
-        response = self.view(request)
+        response = self.client.post(reverse('posts'), sample_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
